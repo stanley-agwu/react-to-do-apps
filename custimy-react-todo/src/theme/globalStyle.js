53914fc22fe1500a2bb02332d825061dd8369655
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-10-31 18:04:03 +0100

    Added global styles and global theme variables, named app in index.html, and installed font-awesome free svg icons

--- custimy-react-todo/src/theme/globalStyle.js
+++ custimy-react-todo/src/theme/globalStyle.js
@@ -6,6 +6,16 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: Roboto, sans-serif, serif;
     background: #f8f9fb;
+    margin: 10px 0;
+    padding: 0 5px;
+    list-style: none;
+    box-sizing: border-box;
+
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+    }
   }
 `;
 
@@ -14,6 +24,14 @@ const globalTheme = {
   lightGrey: '#d3d7dd',
   darkerGrey: '#b6bdc7',
   darkestGrey: '#2c3139',
+  OtherRed: '#f7ccbd',
+  darkRed: '#f1a88e',
+  normalRed: '#e60000',
+  lightRed: '#ff6666',
+  blue: '#0080ff',
+  white: '#ffffff',
+  green: '#04aa6d',
+  lightBlue: '#99ccff'
 };
 
 export { GlobalStyle, globalTheme };
